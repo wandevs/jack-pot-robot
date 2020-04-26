79405b983c593d56d91b373780f71601d9a613dd
codeoneline <mutouji1@163.com>
2020-04-26 15:08:58 +0800

    change time again and change to english comments

--- robot.js
+++ robot.js
@@ -6,31 +6,31 @@ log4js.configure('./config/log4js.json');
 const log = log4js.getLogger("app");
 
 const robotSchedules = ()=>{
-  // update，daily
+  // update: The settlement robot calls this function daily to update the capital pool and settle the pending refund.
   schedule.scheduleJob('0 0 6 * * *', async () => {
-    log.info('open a new lottery');
+    log.info('update a lottery');
     await wan.jackPot.update();
   }); 
 
-  // open 开始投注，周六早上8点 
+  // open: open betting every saturday morning
   schedule.scheduleJob('0 0 8 * * 6', async () => {
     log.info('open a new lottery');
     await wan.jackPot.open();
   }); 
 
-  // close 关闭投注，周五早上7点
+  // close: is called regularly by the robot on 4 nights a week to close bets.
   schedule.scheduleJob('0 0 7 * * 5', async () => {
     log.info('close current lottery');
     await wan.jackPot.close();
   });
 
-  // runDelegateIn, 周五晚上8点
-  schedule.scheduleJob('0 0 20 * * 5', async () => {
+  // runDelegateIn: After the settlement is completed, the settlement robot will call this function to conduct POS delegation to the funds in the capital pool that meet the proportion of the commission.
+  schedule.scheduleJob('0 0 20 * * *', async () => {
     log.info('runDelegateIn current lottery');
     await wan.jackPot.runDelegateIn();
   });
 
-  // lotterySettlement 结算, 周五晚上7点
+  // Lottery settlement:  On the Friday night, the robot calls this function to get random Numbers and complete the lucky draw process.
   schedule.scheduleJob('0 0 19 * * 5', async () => {
     log.info('settlement current lottery');
     await wan.jackPot.lotterySettlement();
