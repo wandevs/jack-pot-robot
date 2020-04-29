40502e156bf581b56e091b493dce4e97606a30a3
codeoneline <mutouji1@163.com>
2020-04-29 16:56:23 +0800

    fix bug and change log and change waiting time

--- robot.js
+++ robot.js
@@ -27,9 +27,11 @@ const robotSchedules = ()=>{
   // runDelegateIn: After the settlement is completed, the settlement robot will call this function to conduct POS delegation to the funds in the capital pool that meet the proportion of the commission.
   schedule.scheduleJob('0 0 20 * * *', async () => {
     // check delegate total amount, when > 20000 wan, change validator, if use validators > 5, delegateOut one
-    await wan.jackPot.chooseValidator();
+    const success = await wan.jackPot.chooseValidator();
     log.info('runDelegateIn current lottery');
-    await wan.jackPot.runDelegateIn();
+    if (success) {
+      await wan.jackPot.runDelegateIn();
+    }
   });
 
   // Lottery settlement:  On the Friday night, the robot calls this function to get random Numbers and complete the lucky draw process.
