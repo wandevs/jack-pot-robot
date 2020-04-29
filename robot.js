9e48b0fe066abbbd9739ea94a6290666b553b137
codeoneline <mutouji1@163.com>
2020-04-29 08:46:52 +0800

    add more function

--- robot.js
+++ robot.js
@@ -1,15 +1,18 @@
 const schedule = require('node-schedule');
-// const iWan = require('./lib/iwan');
-const wan = require('./wanchain');
-const log4js = require('log4js');
-log4js.configure('./config/log4js.json');
+const log4js = require('log4js').configure('./config/log4js.json');
 const log = log4js.getLogger("app");
+const wan = require('./lib/wanchain');
+const sendMail = require('./lib/email');
+
 
 const robotSchedules = ()=>{
   // update: The settlement robot calls this function daily to update the capital pool and settle the pending refund.
   schedule.scheduleJob('0 0 6 * * *', async () => {
     log.info('update a lottery');
-    await wan.jackPot.update();
+    const result = await wan.jackPot.update();
+    if (result.status === 0x1) {
+
+    }
   }); 
 
   // open: open betting every saturday morning
@@ -26,6 +29,8 @@ const robotSchedules = ()=>{
 
   // runDelegateIn: After the settlement is completed, the settlement robot will call this function to conduct POS delegation to the funds in the capital pool that meet the proportion of the commission.
   schedule.scheduleJob('0 0 20 * * *', async () => {
+    // check delegate total amount, when > 20000 wan, change validator, if use validators > 5, delegateOut one
+    await wan.pos.chooseValidator();
     log.info('runDelegateIn current lottery');
     await wan.jackPot.runDelegateIn();
   });
@@ -35,6 +40,29 @@ const robotSchedules = ()=>{
     log.info('settlement current lottery');
     await wan.jackPot.lotterySettlement();
   });
+
+  // check contract address's balance >= positive pool's balance per minute
+  schedule.scheduledJobs('30 * * * * *', async () => {
+    const isClose = await wan.jackPot.closed().call();
+    if (isClose) {
+      return;
+    }
+    const success = await wan.jackPot.balanceCheck();
+    if (!success) {
+      // send a mail
+      await sendMail("balance error", "demandDepositPool > contract's balance");
+      // close contract
+      await close();
+    }
+  });
+
+  // daily check if a validator want to exit, send a email, and delegateOut
+  schedule.scheduledJobs('0 0 24 * * *', async () => {
+    log.info('check validator exit');
+    await wan.jackPot.checkStakerOut();
+  });
+
+  //
 };
 
 log.info('running robot');
--- test.js
+++ test.js
@@ -19,4 +19,45 @@
 // │ └──────────────────── minute (0 - 59)
 // └───────────────────────── second (0 - 59, OPTIONAL)
 
+const log = require('./lib/log');
+require('./lib/email');
+const jackPot = require('./lib/wanchain').jackPot;
+const pos = require('./lib/wanchain').pos;
+const getBalance = require('./lib/wanchain').getBalance;
+const loadValidatorsState = require('./lib/wanchain').loadValidatorsState;
+const saveValidatorsState = require('./lib/wanchain').saveValidatorsState;
+const sendMail = require('./lib/email');
+
+log.info("test");
+
+
+
+setTimeout( async () => {
+  // log.info(await getBalance(process.env.JACKPOT_OPERATOR_ADDRESS));
+
+  // await jackPot.open();
+  // await jackPot.update();
+  // await jackPot.lotterySettlement();
+  // await jackPot.close();
+  // await jackPot.runDelegateIn();
+
+  // await jackPot.balanceCheck();
+
+  // await sendMail("HelloWorld", "this is an error");
+
+  // await pos.getStakerInfo(73665);
+
+  // const dataOrigin = {
+  //   "0x23fc2eda99667fd3df3caa7ce7e798d94eec06eb": 1,
+  //   "0x435b316a70cdb8143d56b3967aacdb6392fd6125": 0,
+  //   "0x2d0e7c0813a51d3bd1d08246af2a8a7a57d8922e": 1};
+  // saveValidatorsState(dataOrigin);
+  // const data = loadValidatorsState();
+  // console.log(JSON.stringify(data));
+
+  // await jackPot.getValidatorsInfo();
+
+  await jackPot.checkStakerOut();
+
+}, 0);
 
